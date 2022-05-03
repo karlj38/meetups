b47cee6d8874968426f8e3fe3b29367b9f86e08a
Karl Jones <karlj38@gmail.com>
2022-05-03 15:52:35 +0100

    add cta buttons

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,3 +1,27 @@
 <template>
-  <p>Home Page</p>
+  <v-container>
+    <v-row wrap>
+      <v-col class="col-sm-6 text-sm-right text-center">
+        <v-btn
+          class="info"
+          large
+          to="/meetups"
+          router
+        >
+          Expore Meetups
+        </v-btn>
+      </v-col>
+
+      <v-col class="col-sm-6 text-sm-left text-center">
+        <v-btn
+          class="info"
+          large
+          to="/meetups/new"
+          router
+        >
+          Create Meetup
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>

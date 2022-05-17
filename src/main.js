d2d662044a769011fc2bd9a94ee1e051c51d3196
Karl Jones <karlj38@gmail.com>
2022-05-17 17:08:20 +0100

    install firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import { store } from "./store"
 import DateFilter from "./filters/date";
+import * as firebase from "firebase/app";
 
 Vue.config.productionTip = false
 
@@ -13,5 +14,16 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBYuSaCWCSPoWHZAlzPFXVL07uah2goCgU",
+      authDomain: "meetups-ad1cc.firebaseapp.com",
+      projectId: "meetups-ad1cc",
+      storageBucket: "meetups-ad1cc.appspot.com",
+      messagingSenderId: "636331269258",
+      appId: "1:636331269258:web:d5bd947a15393c2a6de173",
+      measurementId: "G-055SB0SHEH"
+    })
+  }
 }).$mount('#app')
